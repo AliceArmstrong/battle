d763a38bce746d63090b3df6ebefbea667ff0080
Alice Armstrong <alicearmstrong@Alices-MacBook-Pro.local>
2017-03-02 16:43:59 +0000

    Added HP for player 2

--- app.rb
+++ app.rb
@@ -1,20 +1,31 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     'Hello Battle!'
   end
 
   run! if app_file == $0
 
-  get '/battle/add_players' do
+  get '/add_players' do
     erb(:index)
   end
 
-  post '/battle/names' do
+  post '/names' do
     p params
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session['player1'] = params[:player1]
+    session['player2'] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session['player1']
+    @player2 = session['player2']
+    @player2hp = "20"
+    @maxhp = "100"
     erb(:play)
   end
 
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 RSpec.feature "initialising battle game with 2 players" do
   scenario "allows players to enter their names and see them" do
-    visit "/battle/add_players"
+    visit "/add_players"
     fill_in("player1", with: "Alice")
     fill_in("player2", with: "Noora")
     click_button("Submit")
     expect(page).to have_text("Alice" && "Noora")
   end
+end
+
+RSpec.feature "recording HPs" do
+    scenario "shows player 2s HP" do
+      visit "/add_players"
+      fill_in("player1", with: "Alice")
+      fill_in("player2", with: "Noora")
+      click_button("Submit")
+      expect(page).to have_css("progress[value = '20']")
+    end
 end
