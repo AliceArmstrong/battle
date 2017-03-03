17818e5ae8bc41a5a00c4b91a59f61bed34deab2
Christos <chris.paraskeva10@gmail.com>
2017-03-03 15:50:40 +0000

    added player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,8 @@
+require 'player'
+
+describe Player do
+  it 'checks that an instance of the class has been created' do
+    players = Player.new(player1, player2)
+    expect(players).to eq players
+  end
+end
